74a5267b7c64fe3e66bd2c6b75de7965371d8392
José G. Páez <jpaez@upv.edu.es>
2024-01-31 15:21:20 +0100

    Función de inicio para worknats

---
+++ worker/workNats.js
@@ -0,0 +1,17 @@
+function codeExecution() {
+    if (petition.file.toString().includes(".js")) {
+      shell.exec("cd dir && npm install", { silent: true });
+      execution = shell.exec("node ./dir" + petition.path + "/" + petition.file + " " + petition.arguments, { silent: true });
+      if (execution.code !== 0) {
+        throw new Error(execution.stderr);
+      }
+    } else if (petition.file.toString().includes(".py")) {
+      shell.exec("cd dir && pip install -r requirements.txt", { silent: true });
+      execution = shell.exec("python3 ./dir" + petition.path + "/" + petition.file + " " + petition.arguments, { silent: true });
+      if (execution.code !== 0) {
+        throw new Error(execution.stderr);
+      }
+    } else {
+      throw new Error('Tipo de fichero no soportado');
+    }
+  }
